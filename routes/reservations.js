143a792a8c60fc7f38e879f1d5a2ef89c8fe8621
Alex Hanson <alexhanson@Alexs-MacBook-Air.local>
2020-03-09 14:52:59 -0700

    protect routes with user authentication

--- routes/reservations.js
+++ routes/reservations.js
@@ -2,12 +2,18 @@ const reservationsCtrl = require('../controllers/reservations');
 const express = require('express');
 const router = express.Router();
 
-router.get('/hotels/:id/reservations/new/:error', reservationsCtrl.new );
+router.get('/hotels/:id/reservations/new/:error', isLoggedIn, reservationsCtrl.new );
 router.get('/reservations/:id', reservationsCtrl.show);
 
 router.post('/hotels/:id/reservations', reservationsCtrl.create);
 
-router.put('/reservations/:id', reservationsCtrl.update);
-router.delete('/reservations/:id', reservationsCtrl.delete);
+router.put('/reservations/:id',isLoggedIn, reservationsCtrl.update);
+router.delete('/reservations/:id', isLoggedIn, reservationsCtrl.delete);
 
 module.exports = router;
+
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+  }
